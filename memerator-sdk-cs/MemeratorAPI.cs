0224d3f04a72af36af67b72265b0c23c09eec8cd
Chew <chew@chew.pw>
2020-08-10 01:41:00 -0500

    Base files

---
+++ memerator-sdk-cs/MemeratorAPI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json;
+using memerator_sdk_cs.Objects;
+
+namespace memerator_sdk_cs
+{
+    public class MemeratorAPI
+    {
+        public static String token;
+        public static API api;
+
+        public MemeratorAPI(String key)
+        {
+            token = key;
+        }
+        
+        public void setToken(String newToken) 
+        {
+            token = newToken;
+            api = new API(newToken);
+        }
+        
+        public API getAPI()
+        {
+            return api;
+        }
+
+        public Meme getMeme(String id)
+        {
+            return JsonSerializer.Deserialize<Meme>(getAPI().get("meme/" + id));
+        }
+    }
+}
