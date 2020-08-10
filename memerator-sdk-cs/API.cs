d81b53ce1c60cbcb87d701092817264400891db7
Chew <chew@chew.pw>
2020-08-10 01:41:17 -0500

    API file

---
+++ memerator-sdk-cs/API.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace memerator_sdk_cs
+{
+    public class API
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public API(String apiKey)
+        {
+            client.BaseAddress = new Uri("https://api.memerator.me/v1/");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(apiKey);
+        }
+
+        public String get(String path) {
+            return client.GetStringAsync(path).Result;
+        }
+
+        public String post(String path, Dictionary<String, String> values)
+        {
+            var content = new FormUrlEncodedContent(values);
+            var response = client.PostAsync(path, content).Result;
+
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public String put(String path, Dictionary<String, String> values)
+        {
+            var content = new FormUrlEncodedContent(values);
+            var response = client.PutAsync(path, content).Result;
+
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public String patch(String path, Dictionary<String, String> values)
+        {
+            var content = new FormUrlEncodedContent(values);
+            var response = client.PatchAsync(path, content).Result;
+
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public String delete(String path)
+        {
+            return client.DeleteAsync(path).Result.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
